2431127b2c30e1f93d629adaa6461076a9fb8e5d
Danylo <dens123456@gmail.com>
2023-05-23 22:10:08 +0200

    fix

--- AparatLabs.Lab3/Program.cs
+++ AparatLabs.Lab3/Program.cs
@@ -1,23 +1,12 @@
 ﻿using System.Text;
 namespace AparatLabs.Lab3;
 
+
 public static class Program
 {
-    //Метод GetT приймає два параметри - початковий час t0 і множник multiply, і повертає значення часу T, яке використовується в інших розрахунках.
-    private static double GetT(double t0, double multiply) => multiply * t0;
-
-    //Метод CalcKx приймає параметри dx, t, t0 і k, і розраховує значення кореляційної функції Kx за формулою (3.9) з вхідних даних.
-    private static double CalcKx(double dx, double t, double t0, double k) => dx * (1 - Math.Pow(t / t0, k));
-
-    //Метод CalcKxInterpolation приймає параметри dx, t0 і T, і розраховує значення кореляційної функції Kx за формулою (3.11) з вхідних даних.
-    private static double CalcKxInterpolation(double dx, double t0, double T) => dx * Math.Exp(-t0 / T);
-
-    //Метод CalcResult приймає параметри kx, dx, xti і mx, і розраховує значення технологічного параметру за формулою з вхідних даних.
-    private static double CalcResult(double kx, double dx, double xti, double mx) => kx / dx * (xti - mx) + mx;
-
     //У методі Main виконуються основні розрахунки. Спочатку визначається час T за допомогою методу GetT.
-    //Потім розраховуються значення кореляційних функцій Kx за допомогою методів CalcKx і CalcKxInterpolation.
-    //Нарешті, розраховуються значення технологічних параметрів за допомогою методу CalcResult для екстраполяції і інтерполяції.
+    //Потім розраховуються значення кореляційних функцій Kx за допомогою методів.
+    //Нарешті, розраховуються значення технологічних параметрів для екстраполяції і інтерполяції.
     //Результати виводяться на консоль.
     public static void Main()
     {
@@ -36,14 +25,18 @@ public static class Program
         double k = 1.5;
         //Період прогнозу, t (момент часу, в котрий необхідно визначити величину)
         double multiply = 0.5;
-        
-        double T = GetT(t0, multiply);
 
-        double kx = CalcKx(dx, T, t0, k);
-        double kxInterpolation = CalcKxInterpolation(dx, t0, T);
+        //повертає значення часу T, яке використовується в інших розрахунках
+        double T = multiply * t0;
 
-        double extrapolation = CalcResult(kx, dx, xti, mx);
-        double interpolation = CalcResult(kxInterpolation, dx, xti, mx);
+        //розраховує значення кореляційної функції Kx за формулою (3.9) з вхідних даних
+        double kx = dx * (1 - Math.Pow(T / t0, k));
+        //розраховує значення кореляційної функції Kx за формулою (3.11) з вхідних даних.
+        double kxInterpolation = dx * Math.Exp(-t0 / T);
+
+        //розраховує значення технологічного параметру за формулою з вхідних даних.
+        double extrapolation = kx / dx * (xti - mx) + mx;
+        double interpolation = kxInterpolation / dx * (xti - mx) + mx;
 
         Console.WriteLine($"T = {T:F4}");
         Console.WriteLine($"Kx екстраполяції = {kx:F4}");
